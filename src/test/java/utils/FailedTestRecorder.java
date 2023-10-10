ca8810c92c9f6c24780aae93fe3cc6f669665af2
PAVEL_YAKOVLEV <today_na@mail.ru>
2023-10-10 11:50:02 +0300

    Задача U8. Перезапуск упавших тестов. FaledTestRecorder refactoring

--- src/test/java/utils/FailedTestRecorder.java
+++ src/test/java/utils/FailedTestRecorder.java
@@ -3,19 +3,20 @@ package utils;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
+import java.nio.file.Path;
 import java.nio.file.Paths;
 
 public class FailedTestRecorder {
-    private static final String FILE_PATH = ConfProperties.getProperty("faledTestsBatName");
 
     public static void recordFailedTest(String className, String methodName) throws IOException {
         String methodNameWithoutParentheses = methodName.replaceAll("\\([^()]*\\)", "");
         String testName = className + "#" + methodNameWithoutParentheses + ",";
-        String line = Files.readString(Paths.get(FILE_PATH), StandardCharsets.UTF_8);
+        Path path = Paths.get(ConfProperties.getProperty("failedTestsBatName"));
+        String line = Files.readString(path, StandardCharsets.UTF_8);
         if (line != null && !line.contains(className + "#" + methodNameWithoutParentheses)) {
-            Files.writeString(Paths.get(FILE_PATH), testName, StandardCharsets.UTF_8);
+            Files.writeString(path, testName, StandardCharsets.UTF_8);
         } else {
-            Files.writeString(Paths.get(FILE_PATH), "mvn test -Dtest=" + testName, StandardCharsets.UTF_8);
+            Files.writeString(path, "mvn test -Dtest=" + testName, StandardCharsets.UTF_8);
         }
     }
 }
