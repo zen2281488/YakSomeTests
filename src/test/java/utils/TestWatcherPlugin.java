aadf4141811295899136901d7e1664fc5fd46bef
PAVEL_YAKOVLEV <today_na@mail.ru>
2023-10-10 15:33:50 +0300

    Задача U8. Перезапуск упавших тестов. FaledTestRecorder refactoring

--- src/test/java/utils/TestWatcherPlugin.java
+++ src/test/java/utils/TestWatcherPlugin.java
@@ -4,15 +4,26 @@ import org.junit.jupiter.api.extension.ExtensionContext;
 import org.junit.jupiter.api.extension.TestWatcher;
 import org.openqa.selenium.WebDriver;
 
+import java.io.IOException;
 import java.lang.reflect.Field;
 
-public class TestWatcherPlugin implements TestWatcher {
 
+
+public class TestWatcherPlugin implements TestWatcher {
+    private boolean isFirstFailure = true;
     @Override
     public void testFailed(ExtensionContext context, Throwable cause) {
         WebDriver driver = getWebDriverFromTestInstance(context.getRequiredTestInstance());
         if (driver != null) {
             ScreenShot.captureScreenshot(driver);
+        }
+        if(isFirstFailure){
+            try {
+                FailedTestRecorder.newFailedTestFile();
+                isFirstFailure = false;
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
         }
     }
 
