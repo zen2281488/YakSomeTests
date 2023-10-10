f1747f2450a76a9cf9e1f65ea3299d506c5951a3
PAVEL_YAKOVLEV <today_na@mail.ru>
2023-10-10 11:36:07 +0300

    Задача U8. Перезапуск упавших тестов. FaledTestRecorder refactoring

--- src/test/java/utils/FailedTestRecorder.java
+++ src/test/java/utils/FailedTestRecorder.java
@@ -1,7 +1,5 @@
 package utils;
 
-import java.io.BufferedWriter;
-import java.io.FileWriter;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
@@ -10,24 +8,18 @@ import java.nio.file.Paths;
 public class FailedTestRecorder {
     private static final String FILE_PATH = "failed_tests.bat";
 
-    public static void recordFailedTest(String className, String methodName) {
+    public static void recordFailedTest(String className, String methodName) throws IOException {
         String methodNameWithoutParentheses = methodName.replaceAll("\\([^()]*\\)", "");
         String testName = className + "#" + methodNameWithoutParentheses + ",";
-        try (BufferedWriter bw = new BufferedWriter(new FileWriter(FILE_PATH, true))) {
-            String line = Files.readString(Paths.get(FILE_PATH), StandardCharsets.UTF_8);
-            if (line != null) {
-                if (!line.contains(className + "#" + methodNameWithoutParentheses)) {
-                    bw.write(testName);
-                }
-
-            } else {
-                bw.write("mvn test -Dtest=" + testName);
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
+        String line = Files.readString(Paths.get(FILE_PATH), StandardCharsets.UTF_8);
+        if (!line.contains(className + "#" + methodNameWithoutParentheses) & line != null) {
+            Files.writeString(Paths.get(FILE_PATH), testName, StandardCharsets.UTF_8);
+        } else {
+            Files.writeString(Paths.get(FILE_PATH), "mvn test -Dtest=" + testName, StandardCharsets.UTF_8);
         }
     }
 }
+
 
 
 
