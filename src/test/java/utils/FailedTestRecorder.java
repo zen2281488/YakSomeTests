8b063ee01ecd839e9bc338b36f215d6c9e736d2d
PAVEL_YAKOVLEV <today_na@mail.ru>
2023-10-10 11:51:25 +0300

    Задача U8. Перезапуск упавших тестов. FaledTestRecorder refactoring

--- src/test/java/utils/FailedTestRecorder.java
+++ src/test/java/utils/FailedTestRecorder.java
@@ -7,11 +7,11 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 
 public class FailedTestRecorder {
+    static Path path = Paths.get(ConfProperties.getProperty("failedTestsBatName"));
 
     public static void recordFailedTest(String className, String methodName) throws IOException {
         String methodNameWithoutParentheses = methodName.replaceAll("\\([^()]*\\)", "");
         String testName = className + "#" + methodNameWithoutParentheses + ",";
-        Path path = Paths.get(ConfProperties.getProperty("failedTestsBatName"));
         String line = Files.readString(path, StandardCharsets.UTF_8);
         if (line != null && !line.contains(className + "#" + methodNameWithoutParentheses)) {
             Files.writeString(path, testName, StandardCharsets.UTF_8);
