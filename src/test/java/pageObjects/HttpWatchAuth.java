d66a07318471a5f3a1c0ebc8a3011ef8216d23bb
zen2281488 <today_na@mail.ru>
2023-12-30 15:18:21 +0300

    U13 Basic Auth added

--- src/test/java/pageObjects/HttpWatchAuth.java
+++ src/test/java/pageObjects/HttpWatchAuth.java
@@ -25,7 +25,9 @@ public class HttpWatchAuth extends BasePage {
 
     @Step("Отправляем логин и пароль в поле авторизации")
     public HttpWatchAuth sendLogPass() {
-        browser.get("https://" + ConfProperties.getProperty("httpwatchLogin") + ":" + ConfProperties.getProperty("httpwatchPassword") + "@www.httpwatch.com/httpgallery/authentication/#showExample10");
+        browser.get("https://" + ConfProperties.getProperty("httpwatchLogin")
+                + ":" + ConfProperties.getProperty("httpwatchPassword")
+                + "@www.httpwatch.com/httpgallery/authentication/#showExample10");
         return this;
     }
 
