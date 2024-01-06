6a3ecd8bad629b355ccaf49e7035be6e36f3034e
zen2281488 <today_na@mail.ru>
2024-01-06 12:08:00 +0300

    added Hooks and TestContext for init. Webdriver

--- src/test/java/steps/DataProviderAuthTestSteps.java
+++ src/test/java/steps/DataProviderAuthTestSteps.java
@@ -8,13 +8,10 @@ import utils.ConfProperties;
 
 public class DataProviderAuthTestSteps {
 
-    private WayAutorisation wayAutorisation;
-
     private TestContext testContext;
 
     public DataProviderAuthTestSteps() {
         this.testContext = new TestContext();
-        wayAutorisation = new WayAutorisation(testContext.getDriver());
     }
 
     @Given("User is on the login page")
@@ -24,11 +21,11 @@ public class DataProviderAuthTestSteps {
 
     @When("User logs in with username {string}, description {string} and password {string}")
     public void userLogsIn(String username, String description, String loginPassword) {
-        wayAutorisation.sendUsername(username).sendPassword(loginPassword).sendUserDescription(description).clickSubmitButton();
+        new WayAutorisation(testContext.getDriver()).sendUsername(username).sendPassword(loginPassword).sendUserDescription(description).clickSubmitButton();
     }
 
     @Then("Successful login message is displayed")
     public void successfulLoginMessageIsDisplayed() {
-        wayAutorisation.waitSuccessLoginText();
+        new WayAutorisation(testContext.getDriver()).waitSuccessLoginText();
     }
 }
--- src/test/java/steps/DisplayTestSteps.java
+++ src/test/java/steps/DisplayTestSteps.java
@@ -8,37 +8,34 @@ import utils.ConfProperties;
 
 public class DisplayTestSteps {
 
-    private WayIndexPage wayIndexPage;
-
     private TestContext testContext;
 
     public DisplayTestSteps() {
         this.testContext = new TestContext();
-        wayIndexPage = new WayIndexPage(testContext.getDriver());
     }
 
     @Step("Слайдер курсов существует.")
     @Then("Slider of Courses displayed")
     public boolean sliderCourcesDisplayed() {
-        return wayIndexPage.sliderCourcesDisplayed();
+        return new WayIndexPage(testContext.getDriver()).sliderCourcesDisplayed();
     }
 
     @Step("Футер существует.")
     @Then("Footer should be displayed")
     public boolean footerDisplayed() {
-        return wayIndexPage.footerDisplayed();
+        return new WayIndexPage(testContext.getDriver()).footerDisplayed();
     }
 
     @Step("Блок с сертификатами существует.")
     @Then("Certificates block should be displayed")
     public boolean certificatesBlockDisplayed() {
-        return wayIndexPage.certificatesBlockDisplayed();
+        return new WayIndexPage(testContext.getDriver()).certificatesBlockDisplayed();
     }
 
     @Step("Блок с контактами существует.")
     @Then("Contacts block displayed")
     public boolean contactsDisplayed() {
-        return wayIndexPage.contactsDisplayed();
+        return new WayIndexPage(testContext.getDriver()).contactsDisplayed();
     }
 
     @Given("I am on the main page")
